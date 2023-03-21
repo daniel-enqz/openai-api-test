0a0d40562e1c56f35243e9c23a1249a34ee57acf
Daniel Enriquez <daniel.enm17@gmail.com>
2023-03-21 16:15:20 -0600

    add main config

--- app.py
+++ app.py
@@ -23,13 +23,4 @@ def index():
 
 
 def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+    return "Suggest five names for an Anime Woman Character, you should use Olivia F."
